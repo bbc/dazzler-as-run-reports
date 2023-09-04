eeadf49739196c3d36d9f7d84e63a08b4ec61a05
jcable <julian.cable@bbc.co.uk>
2023-09-04 20:22:09 +0100

    fix: progress

--- process.py
+++ process.py
@@ -1,59 +1,87 @@
 import json
-import sys
-from datetime import datetime, timedelta
+import os
+from datetime import datetime, timedelta, timezone
 from isoduration import parse_duration, format_duration
 import requests
 import csv
+import boto3
+import time
+import pandas as pd
+
+apiKey = os.environ['API_KEY']
+
+client = boto3.client('logs', region_name='eu-west-2')
+
+episodes = {}
+clips = {}
 
 def round_seconds(obj: datetime) -> datetime:
     if obj.microsecond >= 500_000:
         obj += timedelta(seconds=1)
     return obj.replace(microsecond=0)
 
-def v2e(vpid):
-    r = requests.get(f'https://localhost:8443/version/_doc/pid.{vpid}', verify=False)
+def resolve(vpid):
+    r = requests.get(f'http://programmes.api.bbc.com/nitro/api/programmes?api_key={apiKey}&version={vpid}', headers={'Accept': 'application/json'})
     a = r.json()
-    if a['found']:
-        return a['_source']['pips']['version']['version_of']['link']['pid']
-    return None
+    return a['nitro']['results']['items'][0]
 
-def episode(pid):
-    r = requests.get(f'https://localhost:8443/episode/episode/pid.{pid}', verify=False)
-    a = r.json()
-    if a['found']:
-        return a['_source']
-    return None
 
 # fields @timestamp, @message
 # | sort @timestamp asc
 # | limit 9999 
 # | filter @message like /"as_run_type": "input_switch_initiated.*url_path/
 
-inp = sys.argv[1]
-ondate = sys.argv[2]
+def unixtime(dt):
+  return 1000*int(time.mktime(datetime.fromisoformat(dt).timetuple()))
 
-outf = inp.replace('.json', f'-{ondate}.csv')
+channel = 'History_Channel'
+channelId = '7510516'
+region = 'eu-west-2'
+account = '739777694307'
+viewers = pd.read_excel('iPlayer Streams Users - History Channel.xlsx')
+k = viewers.keys()
+days = viewers[k[2]]
+print(days)
 
-f = open(inp)
+ml = boto3.client('medialive', region_name='eu-west-2')
 
-data = [row for row in json.load(f) if row['@timestamp'].startswith(ondate)]
+mlArn = [c for c in ml.list_channels()['Channels'] if channel in c['Name']][0]['Arn']
+lsn = f"arn_aws_medialive_{region}_{account}_channel_{channelId}_0_as_run"
+print(lsn)
 
-columns = ['start', 'duration', 'vpid', 'episode pid', 'title', 'direction', 'delta']
+paginator = client.get_paginator('filter_log_events')
+
+response_iterator = paginator.paginate(
+    logGroupName='ElementalMediaLive',
+    logStreamNames=[lsn],
+    startTime=unixtime('2023-08-09T00:00:00Z'),
+    endTime=unixtime('2023-08-10T00:00:00Z'),
+    filterPattern='"input_switch_initiated"',
+    PaginationConfig={
+        'MaxItems': 10000,
+        'PageSize': 100,
+        # 'StartingToken': '',
+    }
+)
+
+columns = ['type', 'start', 'duration', 'vpid', 'pid', 'title', 'direction', 'delta']
 report = []
-for i in data:
-    # row = {c:'' for c in columns}
+for page in response_iterator:
+  for i in page['events']:
     row = {}
-    m = i['@message']
+    timestamp = i["timestamp"]
+    m = json.loads(i['message'])
+    if m['as_run_type'] != 'input_switch_initiated':
+        continue
     actionName = m["action_name"]
-    timestamp = m["timestamp"]
     path = m.get("url_path", '')
     if 'distributionbucket' in path:
         e = path.split('_')
-        row['vpid'] = e[3]
+        vpid = e[3]
     elif 'mp4' in path:
-        row['vpid'] = path.split('/')[-1].split('.')[0]
+        vpid = path.split('/')[-1].split('.')[0]
     else:
-        row['vpid'] = ''
+        vpid = ''
     an = actionName.split(' ')
     if len(an) == 4:
         start, duration, source, pid = an
@@ -62,9 +90,12 @@ for i in data:
         pid = ''
     start = f"{start[0:4]}-{start[4:6]}-{start[6:8]}T{start[9:11]}:{start[11:13]}:{start[13:15]}.{start[16:19]}Z"
     planned = datetime.fromisoformat(start)
-    actual = datetime.fromisoformat(f"{timestamp}Z")
-    row['duration'] = format_duration(parse_duration(duration))
+    actual = datetime.fromtimestamp(int(timestamp/1000), tz=timezone.utc)
+    pdur = parse_duration(duration)
+    row['vpid'] = vpid
+    row['duration'] = format_duration(pdur)
     row['start'] = round_seconds(actual).strftime('%H:%M:%S')
+    row['end'] = round_seconds(actual+pdur).strftime('%H:%M:%S')
     delta = (actual - planned).total_seconds()
     if delta > 0.5:
         row['direction'] = 'late'
@@ -74,15 +105,12 @@ for i in data:
         row['delta'] = -delta
     if pid != row.get('vpid', ''):
         print('?', pid, row.get('vpid', ''))
-    epid = v2e(pid)
-    if epid:
-        row['episode pid'] = epid
-        ep = episode(epid)
-        if ep:
-            row['title'] = ep['pips']['episode']['title']['$']
+    entity = resolve(pid)
+    row['type'] = entity['item_type']
+    row['pid'] = entity['pid']
+    row['title'] = entity['title']
     report.append(row)
-f.close()
-with open(outf, 'w', newline='') as output_file:
-    dict_writer = csv.DictWriter(output_file, columns)
-    dict_writer.writeheader()
-    dict_writer.writerows(report)
+
+    sched = pd.DataFrame(report)
+    print(sched)
+    print(sched.keys())
