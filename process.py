b3fd74d298bd2c59cdcd07de4c0f19b893acc7ad
jcable <julian.cable@bbc.co.uk>
2023-10-27 11:33:16 +0100

    fix: tidy

--- process.py
+++ process.py
@@ -16,25 +16,23 @@ def addpids(audience_row, schedule):
     e = audience_row['end']
     pids = schedule.loc[s:e].copy().tz_convert('Europe/London')
     pids['User ID'] = audience_row['User ID']
-    return pids.drop(['start', 'end'], axis=1)
+    return pids
 
 audience_data = sys.argv[1]
 sid = sys.argv[2]
 bucket = sys.argv[3]
 augmented = audience_data.replace('.csv', '-dazzler.xlsx')
-viewers = pd.read_csv(audience_data, header=0, index_col=2, dtype={'a': str, 'b': str, 'c': object, 'd': np.float64, 'e': np.float64}, parse_dates=True)
+viewers = pd.read_csv(audience_data, header=0, index_col=2, dtype={'a': str, 'b': str, 'c': object, 'd': np.float64, 'e': np.int32, 'f': np.float64}, parse_dates=True)
 viewers['start'] = pd.to_datetime(viewers.index).tz_localize('Europe/London')
-# viewers['end'] = viewers['start'] + pd.to_timedelta(viewers['AV Session - Playback time'], unit='ms')
 viewers['end'] = viewers['start'] + pd.to_timedelta(viewers['AV - Playback time'], unit='ms')
 s = viewers['start'].min()
 e = viewers['start'].max()
 sd = s.date().isoformat()
 ed = e.date().isoformat()
 print(f'stats from {sd} to {ed}')
-r=pd.date_range(sd, ed)
 with pd.ExcelWriter(augmented) as writer:
     l = []
-    for d in r:
+    for d in pd.date_range(sd, ed):
         day = d.date().isoformat()
         print(f'processing {day}')
         data = get_planned(bucket, sid, day)
@@ -46,6 +44,7 @@ with pd.ExcelWriter(augmented) as writer:
         for index, row in viewsonday.iterrows():
             views = addpids(row, sched).tz_convert('Europe/London').tz_localize(None)
             l.append(views)
+    print(f'there are {len(viewers)} rows of data and {len(l)} groups of processed data')
     sheet = pd.concat(l)
     sheet = sheet[['User ID', 'epid', 'title']]
     sheet.to_excel(writer, sheet_name=f'{sd}-{ed}', index=True)
