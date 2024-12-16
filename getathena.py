129b5d98f274071bf035eb53df45a69eaf419702
jcable <julian.cable@bbc.co.uk>
2024-12-16 11:16:55 +0000

    fix: script to create parquey files from asruns

--- getathena.py
+++ getathena.py
@@ -9,23 +9,21 @@ playback_time, first_level_themes
 from audience_activity 
 where version_id in('p0gh5q9m','p0gh6cp1', 'p0gh7y12', 'p0ghw0r1', 'p0gh684p', 'p0gh4n67') and dt = '20231124'
 '''
+session = boto3.Session(profile_name='aud')
+athena = session.client('athena')
+paginator = athena.get_paginator('get_query_results')
 
-CLIENT = boto3.client("athena", region_name='eu-west-1')
-RESULT_OUTPUT_LOCATION = "s3://aws-athena-query-results-newssystemintegration/" #/Unsaved/2023/11/27/"
-
-def start_query(PID, DATE):
-    query = f"select dt, version_id, audience_id, visit_start_datetime, event_start_datetime_ms, playback_time, first_level_themes from audience_activity where version_id = '{PID}' and dt = '{DATE}'"
-    response = CLIENT.start_query_execution(
+def start_query(pid, date):
+    query = f"select dt, version_id, audience_id, visit_start_datetime, event_start_datetime_ms, playback_time, first_level_themes from audience_activity where version_id = '{pid}' and dt = '{date}'"
+    response = athena.start_query_execution(
         QueryString=query,
-        #ResultConfiguration={"OutputLocation": RESULT_OUTPUT_LOCATION},
-        WorkGroup='dazzler'
+        WorkGroup='dazzler',
+        QueryExecutionContext={'Database':'audience'}
     )
-
     return response["QueryExecutionId"]
 
 def get_data(pid, date):
-    dt = date.replace('-', '')
-    pass
-
-print(get_data('p0gh5q9m', '20231125'))
-
+    qeid = start_query(pid, date)
+    response_iterator = paginator.paginate(QueryExecutionId=qeid)
+    for page in response_iterator:
+        print(page)
--- ml_asruns.py
+++ ml_asruns.py
@@ -1,29 +1,17 @@
 import json
-from datetime import datetime, timedelta
-from isoduration import parse_duration, format_duration
+from datetime import datetime
 import boto3
 import time
-
-def round_seconds(obj: datetime) -> datetime:
-    if obj.microsecond >= 500_000:
-        obj += timedelta(seconds=1)
-    return obj.replace(microsecond=0)
+import sys
+import pandas as pd
 
 def unixtime(dt):
   return 1000*int(time.mktime(dt.timetuple()))
 
-def parseActionName(actionName):
-    an = actionName.split(' ')
-    s = an[0]
-    start_dt = f"{s[0:4]}-{s[4:6]}-{s[6:8]}T{s[9:11]}:{s[11:13]}:{s[13:15]}.{s[16:19]}Z"
-    duration = parse_duration(an[1])
-    return  datetime.fromisoformat(start_dt), duration
-
-def get_as_run(channelId, account, region, start, end):
+def get_as_run(region, lsn, start, end):
     sdt = datetime.fromisoformat(start)
     edt = datetime.fromisoformat(end)
     client = boto3.client('logs', region_name=region)
-    lsn = f"arn_aws_medialive_{region}_{account}_channel_{channelId}_0_as_run"
     paginator = client.get_paginator('filter_log_events')
     response_iterator = paginator.paginate(
         logGroupName='ElementalMediaLive',
@@ -37,36 +25,52 @@ def get_as_run(channelId, account, region, start, end):
             # 'StartingToken': '',
         }
     )
-    report = []
+    return response_iterator
+
+def gen(response_iterator):
     for page in response_iterator:
-        for i in page['events']:
-            row = {}
-            timestamp = i["timestamp"]
-            m = json.loads(i['message'])
-            if m['as_run_type'] != 'input_switch_initiated':
-                continue
-            actionName = m["action_name"]
-            path = m.get("url_path", '')
-            if 'distributionbucket' in path:
-                e = path.split('_')
-                vpid = e[3]
-            elif 'mp4' in path:
-                vpid = path.split('/')[-1].split('.')[0]
-            else:
-                vpid = ''
-            planned, duration = parseActionName(actionName)
-            actual = datetime.fromtimestamp(int(timestamp/1000), tz=sdt.tzinfo)
-            row['timestamp'] = actual.isoformat()
-            row['vpid'] = vpid
-            row['duration'] = format_duration(duration)
-            row['start'] = round_seconds(actual).strftime('%H:%M:%S')
-            # row['end'] = round_seconds(actual+duration).strftime('%H:%M:%S')
-            delta = (actual - planned).total_seconds()
-            if delta > 0.5:
-                row['direction'] = 'late'
-                row['delta'] = delta
-            elif delta < -0.5:
-                row['direction'] = 'early'
-                row['delta'] = -delta
-            report.append(row)
-    return report
+        for line in page['events']:
+            yield json.loads(line['message'])
+
+def cmap(c):
+    return {
+        'name': c['Name'],
+        'arn': c['Arn'],
+        **c['Tags'],
+    }
+
+def get_simple_as_run_report(region, start, end):
+    ml = boto3.client('medialive', region_name=region)
+    channels = [cmap(c) for c in ml.list_channels()['Channels']]
+    for channel in channels:
+        arn = channel['arn']
+        bits = arn.split(':')
+        lsn = f'arn_aws_medialive_{bits[3]}_{bits[4]}_channel_{bits[6]}_0'
+        if 'vpid' in channel:
+            iplayervpid = channel['vpid']
+            name = channel['name'].split(' ')[1]
+        else:
+            print('ignoring', channel['name'])
+            continue
+        response_iterator = get_as_run(region, lsn, start, end)
+        for line in gen(response_iterator):
+            if 'channel_arn' in line:
+                arn = line['channel_arn']
+                message = json.loads(line['message'])
+                action = message['action_name']
+                ts = line['timestamp']
+                planned, duration, source, vpid = action.split(' ')
+                yield {'name': name, 'start': ts, 'duration': duration, 'channel_vpid': iplayervpid, 'item_vpid': vpid}
+
+if __name__ == '__main__':
+    start = sys.argv[1]
+    end = sys.argv[2]
+    irl = pd.DataFrame(get_simple_as_run_report('eu-west-1', start, end))
+    lon = pd.DataFrame(get_simple_as_run_report('eu-west-2', start, end))
+    df = pd.concat([irl,lon])
+    df2=df.astype({'name': 'string', 'start': 'datetime64[ns]', 'duration': 'timedelta64[ns]', 'channel_vpid': 'string', 
+'item_vpid': 'string'})
+    df2['end'] = df2['start'] + df2['duration']
+    df2.to_parquet('p.pq')
+    df3=pd.read_parquet('p.pq')
+    print(df3.info())
