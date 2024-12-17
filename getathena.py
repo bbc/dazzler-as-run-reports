27ff5c476bda5c518eb1243e83c218d8b731a29f
jcable <julian.cable@bbc.co.uk>
2024-12-17 12:59:02 +0000

    fix: get to csv

--- getathena.py
+++ getathena.py
@@ -1,4 +1,7 @@
 import boto3
+import sys
+import json
+import pandas as pd
 
 '''
 select
@@ -13,8 +16,8 @@ session = boto3.Session(profile_name='aud')
 athena = session.client('athena')
 paginator = athena.get_paginator('get_query_results')
 
-def start_query(pid, date):
-    query = f"select dt, version_id, audience_id, visit_start_datetime, event_start_datetime_ms, playback_time, first_level_themes from audience_activity where version_id = '{pid}' and dt = '{date}'"
+def start_query(date):
+    query = f"select version_id, audience_id, event_start_datetime_ms, playback_time from audience_activity where first_level_themes like '%video_pop_up_channel_%' and version_id not like 'video_%' and dt = '{date}'"
     response = athena.start_query_execution(
         QueryString=query,
         WorkGroup='dazzler',
@@ -22,8 +25,24 @@ def start_query(pid, date):
     )
     return response["QueryExecutionId"]
 
-def get_data(pid, date):
-    qeid = start_query(pid, date)
+def get_data(date):
+    qeid = start_query(date)
+    response = athena.get_query_execution(QueryExecutionId = qeid)
+    while response['QueryExecution']['Status']['State'] in ['QUEUED', 'RUNNING']:
+        response = athena.get_query_execution(QueryExecutionId = qeid)
     response_iterator = paginator.paginate(QueryExecutionId=qeid)
     for page in response_iterator:
-        print(page)
+        for row in page['ResultSet']['Rows']:
+            yield [f.get('VarCharValue', '') for f in row['Data']]
+
+if __name__ == '__main__':
+    date = sys.argv[1]
+    l = [r for r in get_data(date)]
+    l = [r for r in l if r[3].isdigit()]
+    # print(json.dumps(l))
+    df = pd.DataFrame(l, columns=['channel_pid', 'audience_id', 'event_start_datetime_ms', 'playback_time'])
+    # df['event_start_datetime_ms'] = df['event_start_datetime_ms'].astype('timestamp64[ns]')
+    df['playback_time'] = df['playback_time'].astype(int)
+    print(df.info())
+    df.to_csv(f'{date}.csv', index=False)
+
