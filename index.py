fdef048aced9471961d1b97237ac13c774ffd9a7
jcable <julian.cable@bbc.co.uk>
2024-12-20 17:27:22 +0000

    fix: tests

--- index.py
+++ index.py
@@ -2,7 +2,6 @@ import json
 from datetime import datetime, timedelta
 import boto3
 import time
-import sys
 import io
 import pandas as pd
 
@@ -55,8 +54,10 @@ def get_simple_as_run_report(region, channels, start, end):
         response_iterator = get_as_run(region, lsn, start, end)
         for line in gen(response_iterator):
             if 'channel_arn' in line:
+                print('LLL', line)
                 arn = line['channel_arn']
                 message = json.loads(line['message'])
+                print('XXXXX', message)
                 action = message['action_name']
                 ts = line['timestamp']
                 planned, duration, source, vpid = action.split(' ')
@@ -72,38 +73,42 @@ def save_csv(df, key):
     df.to_csv(out_buffer, index=False, compression='gzip')
     s3_client.put_object(Bucket='iplayer-dazzler-asruns', Key=f'{key}.csv.gz', Body=out_buffer.getvalue())
 
+def make_one_second_data_for_channel(df, start, end):
+    cp = df[['start', 'channel_vpid', 'item_vpid']]
+    cp.set_index('start', inplace=True)
+    local = cp.tz_convert('Europe/London')
+    range = pd.date_range(start, end, freq='s', tz='Europe/London')
+    car = local.reindex(range, method='pad')
+    car['duration'] = 1
+    return car
+
 def make_one_second_data(channels, start, end, df):
     cd = []
     for channel in channels:
-        vpid = channel['vpid']
-        cp = df.copy()
-        cdf = cp.loc[df['channel_vpid']==vpid]
-        cdf.index = cdf['start']
-        cdf.index = cdf.index.tz_localize('Europe/London')
-        car = cdf.reindex(pd.date_range(start, end, freq='s', tz='Europe/London'), method='pad')
-        ndf = car[['name', 'start', 'channel_vpid', 'item_vpid']].copy()
-        ndf['duration'] = 1
-        cd.append(ndf)
+        cd.append(make_one_second_data_for_channel(df.loc[df['channel_vpid']==channel['vpid']], start, end))
     return pd.concat(cd).dropna()
 
-def lambda_handler(event, context):
-    time = event["time"]
+def main(time, region):
     dt = datetime.fromisoformat(time)
     end = datetime.combine(dt.date(), datetime.min.time())
     start = end - timedelta(hours=24)
-    region = event['region']
     print(region, start, end)
     channels = get_channels(region)
     print(json.dumps(channels))
     df = pd.DataFrame(get_simple_as_run_report(region, channels, start, end))
-    df2=df.astype({'name': 'string', 'start': 'datetime64[ns]', 'duration': 'timedelta64[ns]', 'channel_vpid': 'string', 
-'item_vpid': 'string'})
-    df2['end'] = df2['start'] + df2['duration']
+    df2=df.astype(
+        {'name': 'string', 'start': 'datetime64[ns]', 'duration': 'timedelta64[ns]', 'channel_vpid': 'string', 'item_vpid': 'string'}
+    )
+    e = pd.to_datetime(end).tz_convert('Europe/London')
+    s = pd.to_datetime(start).tz_convert('Europe/London')
+    df3 = make_one_second_data(channels, s, e, df2)
     save_parquet(df2, f'daily/{region}_{start.date().isoformat()}')
     save_csv(df2, f'daily_csv/{region}_{start.date().isoformat()}')
-    df3 = make_one_second_data(channels, start, end, df2)
     save_parquet(df3, f'by_the_second/parquet/{region}/{start.date().isoformat()}')
     save_csv(df3, f'by_the_second/csv/{region}/{start.date().isoformat()}')
+
+def lambda_handler(event, context):
+    main(event["time"], event['region'])
     return {
         'statusCode': 200,
         'body': json.dumps('wrote daily as runs')
---
+++ tests/test_index.py
@@ -0,0 +1,69 @@
+import json
+from moto import mock_aws
+from unittest.mock import patch
+from datetime import datetime
+import pandas as pd
+from index import unixtime, gen, cmap, get_as_run, get_channels, get_simple_as_run_report, make_one_second_data_for_channel, make_one_second_data, main
+
+start = datetime.fromisoformat('2024-12-15T00:00:00Z')
+end = datetime.fromisoformat('2024-12-15T00:02:00Z')
+region = 'eu-west-1'
+
+idf = pd.DataFrame([
+    {'name': 'hist', 'start': '2024-12-15T00:00:00Z', 'duration': 'PT1M', 'channel_vpid': 'cv1', 'item_vpid': 'vp1'},
+    {'name': 'hist', 'start': '2024-12-15T00:01:00Z', 'duration': 'PT1M', 'channel_vpid': 'cv1', 'item_vpid': 'vp2'},
+    {'name': 'xmas', 'start': '2024-12-15T00:00:00Z', 'duration': 'PT1M', 'channel_vpid': 'cv2', 'item_vpid': 'vp3'},
+])
+df=idf.astype(
+    {'name': 'string', 'start': 'datetime64[ns, UTC]', 'duration': 'timedelta64[ns]', 'channel_vpid': 'string', 'item_vpid': 'string'}
+)
+
+def test_unixtime():
+    assert unixtime(datetime.fromisoformat('2023-01-01T00:00:00Z')) == 1672531200000
+
+def test_gen():
+    assert [i for i in gen([])] == []
+    assert [i for i in gen([{'events':[{'message': '""'}]}])] == [""]
+    assert [i for i in gen([{'events':[{'message': '""'}]}, {'events':[{'message': '{}'}]}])] == ["",{}]
+
+
+def test_cmap(): 
+    assert cmap({'Name': 'X Y', 'Arn': 'somearn', 'Tags': {}}) == {'name': 'Y', 'arn': 'somearn'}
+    assert cmap({'Name': 'X Y', 'Arn': 'somearn', 'Tags': {'vpid': 'p'}}) == {'name': 'Y', 'arn': 'somearn', 'vpid': 'p'}
+
+@mock_aws
+def test_get_as_run(): 
+    assert get_as_run(region, 'aaa', start, end) is not None
+
+@mock_aws
+def test_get_channels(): 
+    assert get_channels(region) is not None
+
+@patch('index.get_as_run')
+def test_get_simple_as_run_report_empty(mock_get_as_run): 
+    mock_get_as_run.return_value=[]
+    assert [c for c in get_simple_as_run_report(region, [], start, end)] == []
+    assert [c for c in get_simple_as_run_report(region, [{'arn':':::::::', 'vpid': '', 'name': ''}], start, end)] == []
+
+@patch('index.get_as_run')
+def test_get_simple_as_run_report_not_empty(mock_get_as_run): 
+    message = {'action_name': '20231201000000 PT1M sched avpid'}
+    event = {'channel_arn':'','timestamp':'T','message': json.dumps(message)}
+    mock_get_as_run.return_value=[{'events': [{'message': json.dumps(event)}]}]
+    assert [c for c in get_simple_as_run_report(region, [{'arn':':::::::', 'vpid': '', 'name': ''}], start, end)] == [{'name': '', 'start': 'T', 'duration': 'PT1M', 'channel_vpid': '', 'item_vpid': 'avpid'}]
+
+def test_make_one_second_data_for_channel(snapshot): 
+    input = df[df['channel_vpid']=='cv1']
+    e = pd.to_datetime(end).tz_convert('Europe/London')
+    s = pd.to_datetime(start).tz_convert('Europe/London')
+    r = make_one_second_data_for_channel(input, s, e)
+    snapshot.assert_match(r.to_csv(index_label='start'), 'make_one_second_data_for_channel.csv')
+
+def test_make_one_second_data(snapshot):
+    s = pd.to_datetime(start).tz_convert('Europe/London')
+    e = pd.to_datetime(end).tz_convert('Europe/London')
+    r = make_one_second_data([{'vpid': 'cv1'},{'vpid': 'cv2'}], s, e, df)
+    snapshot.assert_match(r.to_csv(index_label='start'), 'make_one_second_data.csv')
+
+def test_main():
+    pass
